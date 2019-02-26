cf26dc1cb91fa856453ff1307044cbf91c870f1e
Tommy Lau <tlau425@yahoo.com>
2019-02-26 22:09:13 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  popped = array.pop
+  return popped
+end
+
+def pop_with_args
